f4adeb4aacf683295d57f75b835c9d99ae824b86
Son_of_Anton <kris2000_prasob@hotmail.com>
2024-09-20 16:00:58 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const myTitle = document.querySelector('#title');
+myTitle.textContent = `How's the Weather`;
+
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={d779a8839b78ee8e185e27ada76da433}')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
