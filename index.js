852a89d077a0fab98bae955bfd153a87ef8bb795
Son_of_Anton <kris2000_prasob@hotmail.com>
2024-10-09 14:54:47 +0700

    Remove database code

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const myTitle = document.querySelector('#title');
 myTitle.textContent = `How's the Weather`;
 
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={d779a8839b78ee8e185e27ada76da433}')
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d779a8839b78ee8e185e27ada76da433')
 .then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+.then(data => {console.log(data);
+
+console.log(data.clouds.all)
+const cloudData = data.clouds;
+const cloudLvl = document.createElement('h2');  
+if(cloudData.all > 90) cloudLvl.textContent = 'Cloudy af';
+document.body.appendChild(cloudLvl);
+})
+.catch(error => console.error('Error:', error)); 
+
+
+
+
